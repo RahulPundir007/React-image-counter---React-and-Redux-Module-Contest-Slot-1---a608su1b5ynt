236c90a781e6cba7f3ea309ffb50089ec6bcb47e
Rahul <110375120+RahulPundir007@users.noreply.github.com>
2023-10-15 09:55:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 import star from '../star.png'
 const App = () => {
+
+  const [val, setVal] = useState(300);
+
+  function handleIncrease(){
+    setVal(()=>val+2)
+    cons
+  }
+
   return (
     <div id="main">
-      <img src={star}  height="300px" width="300px" />
+      <img src={star}  height={`${val} + px`} width={`${val} + px`} onClick={handleIncrease} />
     </div>
   )
 }
